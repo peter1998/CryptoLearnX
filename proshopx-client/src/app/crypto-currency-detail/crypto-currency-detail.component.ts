b562627dccbe5e85706274232f8e6c663fb915ba
Peter Matov <pmatov@gmail.com>
2023-05-30 04:31:38 +0300

    [C] Basic functionality working

--- proshopx-client/src/app/crypto-currency-detail/crypto-currency-detail.component.ts
+++ proshopx-client/src/app/crypto-currency-detail/crypto-currency-detail.component.ts
@@ -1,3 +1,5 @@
+// crypto-currency-detail.component.ts
+
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CryptoCurrencyService } from '../crypto-currency.service';
@@ -10,31 +12,45 @@ import { WatchlistService } from '../watchlist.service';
 })
 export class CryptoCurrencyDetailComponent implements OnInit {
   cryptoCurrency: any;
-  items: any[] = [];
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private watchlistService: WatchlistService,
-    private cryptoCurrencyService: CryptoCurrencyService
+    private cryptoCurrencyService: CryptoCurrencyService,
+    private watchlistService: WatchlistService
   ) {}
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
-    if (id) {
+    if (id !== null) {
       this.cryptoCurrencyService.getCryptoCurrency(id).subscribe(
         (data) => {
           this.cryptoCurrency = data;
+          console.log(this.cryptoCurrency);
         },
         (error) => {
           console.error('Error fetching cryptocurrency', error);
         }
       );
     }
-    this.items = this.watchlistService.getItems();
   }
 
-  addToWatchlist(cryptoCurrency: any) {
-    this.watchlistService.addToWatchlist(cryptoCurrency);
+  // crypto-currency-detail.component.ts
+  // crypto-currency-detail.component.ts
+  addToWatchlist() {
+    const watchlistItem = {
+      id: this.cryptoCurrency.id,
+      name: this.cryptoCurrency.name,
+      current_price: this.cryptoCurrency.market_data.current_price.usd,
+      image: this.cryptoCurrency.image.thumb,
+      market_cap: this.cryptoCurrency.market_data.market_cap.usd,
+      total_volume: this.cryptoCurrency.market_data.total_volume.usd,
+      price_change_percentage_24h:
+        this.cryptoCurrency.market_data.price_change_percentage_24h,
+      market_cap_rank: this.cryptoCurrency.market_cap_rank,
+      // add all other properties required by the CryptoCurrency interface
+    };
+    this.watchlistService.addToWatchlist(watchlistItem);
+    this.router.navigate(['/watchlist']);
   }
 }
