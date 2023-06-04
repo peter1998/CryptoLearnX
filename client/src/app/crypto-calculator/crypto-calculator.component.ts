e98517f84fda255062d967dc64fae37f75aba064
Peter Matov <pmatov@gmail.com>
2023-06-04 23:23:17 +0300

    [C] modify on the crypto-calulator css template

--- client/src/app/crypto-calculator/crypto-calculator.component.ts
+++ client/src/app/crypto-calculator/crypto-calculator.component.ts
@@ -3,6 +3,7 @@ import {
   CryptoCurrencyService,
   CryptoCurrency,
 } from '../crypto-currency.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-crypto-calculator',
@@ -19,17 +20,29 @@ export class CryptoCalculatorComponent implements OnInit {
   constructor(private cryptoCurrencyService: CryptoCurrencyService) {}
 
   ngOnInit(): void {
-    this.cryptoCurrencyService.getCryptocurrencies().subscribe((cryptos) => {
-      this.cryptos = cryptos;
-      this.crypto1 = this.cryptos[0];
-      this.crypto2 = this.cryptos[1];
-    });
+    this.cryptoCurrencyService
+      .getCryptocurrencies()
+      .pipe(
+        tap((cryptos) => {
+          this.cryptos = cryptos;
+          this.crypto1 = this.cryptos[0];
+          this.crypto2 = this.cryptos[1];
+        })
+      )
+      .subscribe();
   }
 
   calculate(): void {
+    console.log('calculate method triggered');
     if (this.crypto1 && this.crypto2) {
-      this.result =
-        (this.amount * this.crypto1.current_price) / this.crypto2.current_price;
+      const crypto1Price = Number(this.crypto1.current_price);
+      const crypto2Price = Number(this.crypto2.current_price);
+      console.log('crypto1Price:', crypto1Price);
+      console.log('crypto2Price:', crypto2Price);
+      if (!isNaN(crypto1Price) && !isNaN(crypto2Price)) {
+        this.result = (this.amount * crypto1Price) / crypto2Price;
+        console.log('result:', this.result);
+      }
     }
   }
 }
