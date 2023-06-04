565af89363c1562ccaeb19a842cb869c6866bd5e
Peter Matov <pmatov@gmail.com>
2023-06-04 22:55:33 +0300

    [C] functionality of crypto basic calculator

--- client/src/app/crypto-calculator/crypto-calculator.component.ts
+++ client/src/app/crypto-calculator/crypto-calculator.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  CryptoCurrencyService,
+  CryptoCurrency,
+} from '../crypto-currency.service';
 
 @Component({
   selector: 'app-crypto-calculator',
   templateUrl: './crypto-calculator.component.html',
-  styleUrls: ['./crypto-calculator.component.css']
+  styleUrls: ['./crypto-calculator.component.css'],
 })
-export class CryptoCalculatorComponent {
+export class CryptoCalculatorComponent implements OnInit {
+  cryptos: CryptoCurrency[] = [];
+  crypto1: CryptoCurrency | null = null;
+  crypto2: CryptoCurrency | null = null;
+  amount = 1;
+  result: number = 0;
 
+  constructor(private cryptoCurrencyService: CryptoCurrencyService) {}
+
+  ngOnInit(): void {
+    this.cryptoCurrencyService.getCryptocurrencies().subscribe((cryptos) => {
+      this.cryptos = cryptos;
+      this.crypto1 = this.cryptos[0];
+      this.crypto2 = this.cryptos[1];
+    });
+  }
+
+  calculate(): void {
+    if (this.crypto1 && this.crypto2) {
+      this.result =
+        (this.amount * this.crypto1.current_price) / this.crypto2.current_price;
+    }
+  }
 }
