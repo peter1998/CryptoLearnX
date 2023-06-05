bf0d286a19491516963c2fbe6e9dfc4732cfe209
Tundzhay Mollov <graculapsone1998@gmail.com>
2023-06-05 22:14:29 +0300

    Update crypto-news.service.ts

--- client/src/app/crypto-news.service.ts
+++ client/src/app/crypto-news.service.ts
@@ -3,7 +3,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/enviroments/environment';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
