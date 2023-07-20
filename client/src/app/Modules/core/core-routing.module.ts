2bfe9aea4c7843ebfb0a0b2132b13132d0568e24
Tundzhay Mollov <graculapsone1998@gmail.com>
2023-07-20 23:31:59 +0300

    added nof-found route in core module

--- client/src/app/Modules/core/core-routing.module.ts
+++ client/src/app/Modules/core/core-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './Pages/homepage/homepage.component';
+import { NotFoundComponent } from './Pages/not-found/not-found.component';
 
-const routes: Routes = [{ path: '', component: HomepageComponent }];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- client/src/app/Modules/core/core.module.ts
+++ client/src/app/Modules/core/core.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { HomepageComponent } from './Pages/homepage/homepage.component';
+import { LandingBoardComponent } from './Components/landing-board/landing-board.component';
+import { NotFoundComponent } from './Pages/not-found/not-found.component';
 
 @NgModule({
-  declarations: [HomepageComponent],
+  declarations: [HomepageComponent, LandingBoardComponent, NotFoundComponent],
   imports: [CommonModule, CoreRoutingModule],
 })
 export class CoreModule {}
