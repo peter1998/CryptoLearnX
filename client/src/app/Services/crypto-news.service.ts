c7c54687aff17a79512b3ca0a30e3ab788792130
Peter Matov <pmatov@gmail.com>
2023-07-29 12:17:35 +0300

    [C] fixing typo

--- client/src/app/Services/crypto-news.service.ts
+++ client/src/app/Services/crypto-news.service.ts
@@ -3,7 +3,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../enviroments/environment';
 
 @Injectable({
   providedIn: 'root',
