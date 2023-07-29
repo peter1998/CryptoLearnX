dc56f3902fe3fb19e4e2bb75e0c1a88e41b8298d
Peter Matov <pmatov@gmail.com>
2023-07-29 12:06:36 +0300

    [C] environemnt import fix - typo

---
+++ client/src/app/crypto-news.service.ts
@@ -0,0 +1,22 @@
+// crypto-news.service.ts
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../enviroments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CryptoNewsService {
+  private url =
+    'https://api.polygon.io/v2/reference/news?limit=10&order=descending&sort=published_utc&ticker=';
+
+  constructor(private http: HttpClient) {}
+
+  getNews(ticker: string): Observable<any> {
+    return this.http.get(
+      this.url + ticker + '&apiKey=' + environment.PolygonApiKey
+    );
+  }
+}
