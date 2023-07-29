eb5b86c477ce2db24c981f5c71d0d9d56ce8c099
Peter Matov <pmatov@gmail.com>
2023-07-29 13:30:14 +0300

    [C] okey

--- client/src/app/Services/crypto-news.service.ts
+++ client/src/app/Services/crypto-news.service.ts
@@ -3,7 +3,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../enviroments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
